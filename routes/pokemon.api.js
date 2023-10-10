4e9d0bb3aaf78b9d1d266603a7a201156409f095
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-10-10 16:34:27 +0700

    :construction:  API for updating a Pokémon

--- routes/pokemon.api.js
+++ routes/pokemon.api.js
@@ -189,4 +189,50 @@ router.post("/", (req, res, next) => {
   }
 });
 
+router.put("/:pokemonId", (req, res, next) => {
+  //----------------------put input validation----------------------
+  try {
+    const allowUpdate = ["name", "id", "type", "url"];
+
+    const { pokemonId } = req.params;
+
+    const updates = req.body;
+    const updateKeys = Object.keys(updates);
+    //find update request that not allow
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    //----------------------put processing----------------------
+    //Read data from db.json then parse to JSobject
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { data } = db;
+    //find book by id
+    const targetIndex = data.findIndex(
+      (pokemon) => pokemon.id === parseInt(pokemonId)
+    );
+    if (targetIndex < 0) {
+      const exception = new Error(`Pokemon not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+    //Update new content to db book JS object
+    const updatedPokemon = { ...db.data[targetIndex], ...updates };
+    db.data[targetIndex] = updatedPokemon;
+
+    //db JSobject to JSON string
+
+    db = JSON.stringify(db);
+    //write and save to db.json
+    fs.writeFileSync("db.json", db);
+    //----------------------put send response----------------------
+    res.status(200).send(updatedPokemon);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
