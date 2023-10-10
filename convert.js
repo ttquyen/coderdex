da31a7ee41eb055f673d1ff68df1dd6c65c70c2e
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-10-10 11:08:55 +0700

    :tada: Add src

---
+++ convert.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+const convertData = async () => {
+  let newData = await csv().fromFile("pokemon.csv"); //return array of JSON
+  newData = new Set(newData.map((e) => e));
+  newData = Array.from(newData);
+  let data = JSON.parse(fs.readFileSync("db.json"));
+  newData = newData
+    .map((e, index) => ({
+      id: index + 1,
+      name: e.Name,
+      type: e.Type2
+        ? [e.Type1.toLowerCase(), e.Type2.toLowerCase()]
+        : [e.Type1.toLowerCase()],
+      url: `http://localhost:5500/images/pokemon/${index + 1}.png`,
+    }))
+    .filter((p) => p.id < 722);
+  //   console.log(newData);
+  data.data = newData;
+  data.totalPokemons = newData.length;
+  fs.writeFileSync("db.json", JSON.stringify(data));
+};
+convertData();
